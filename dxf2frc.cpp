ffe9cb9cffc8d589df2cff4e6561776cb99ea8d5
Aurle Gagne <aurle.gagne@diversey.com>
2021-03-21 20:02:06 -0400

    created an alternate version to generate drive straight distances and heading rotations for FRC

---
+++ dxf2frc.cpp
@@ -0,0 +1,84 @@
+/* Convert a dxf file with a path created with librecad into a path */
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
+
+int max(int first, int second)
+{
+    if (first > second)
+        return first;
+    else
+        return second;
+}
+
+int min(int first, int second)
+{
+    if(first < second)
+        return first;
+    else
+        return second;
+}
+
+int main(int argc, char *argv[]) 
+{
+    char line[50];
+    FILE *dxf_fp = fopen(argv[1],"r");
+    int start_x(0), start_y(0), target_x(0), target_y(0);//, heading(0);
+	float heading(0.0);
+    float last_heading(0.0);
+    printf("{ \"path\": [");
+
+    int first=1;
+    while(fgets(line, sizeof line, dxf_fp))
+    {
+        if (strcmp(line, "AcDbLine\n")==0)
+        {
+            fgets(line,sizeof line, dxf_fp); //Read the 10 after
+            fgets(line,sizeof line, dxf_fp); //Read the x start point
+            sscanf(line, "%d", &start_x);
+
+            fgets(line,sizeof line, dxf_fp); //Read the 20 after
+            fgets(line,sizeof line, dxf_fp); //Read the y start point
+            sscanf(line, "%d", &start_y);
+
+            fgets(line,sizeof line, dxf_fp); //Read the 11 after
+            fgets(line,sizeof line, dxf_fp); //Read the x target point
+            sscanf(line, "%d", &target_x);
+
+            fgets(line,sizeof line, dxf_fp); //Read the 21 after
+            fgets(line,sizeof line, dxf_fp); //Read the y target point
+            sscanf(line, "%d", &target_y);
+
+            int delta_x, delta_y;
+
+            float length = sqrt(  pow( max(target_x, start_x) - min(target_x, start_x), 2) 
+                              + pow( max(target_y, start_y) - min(target_y, start_y), 2) );
+
+            heading = (atan2((target_x-start_x),(target_y-start_y)) * 180 / M_PI);
+
+            if (heading < 0)
+            {
+                heading += 360.00;
+            }
+
+            if (first)
+            {
+                first=0;
+                last_heading = heading;
+            }
+
+            printf("drive straight %f: \n", length);
+            //printf("heading %f \n", heading);
+            printf("rotate %f \n\n", heading - last_heading);
+            last_heading = heading;
+        }
+    }
+    printf("\n]}\n");
+    return 0;
+}
+
+
+
+
